05d9cfd15a9ce644e5c71b67af0b023f357b8e37
ZISHANARIF <zishanarif912@gmail.com>
2025-03-02 16:38:08 +0530

    Test Vercel auto-deployment

--- 01Project/src/app.js
+++ 01Project/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Allow frontend requests
 app.use(cors({
-    origin: ["http://localhost:5173", "https://code-athnsujdc-zishans-projects-56428ae3.vercel.app"], // Update this if your React frontend runs on a different port
+    origin: ["http://localhost:5173", "https://frontend-three-self-43.vercel.app/"], // Update this if your React frontend runs on a different port
     credentials: true,
 }));
 
